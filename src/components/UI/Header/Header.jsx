5eb97dd68692c0d2e788fffe1bc871d521ec09ea
analebyodkina <dp030992cad@gmail.com>
2023-05-15 14:59:18 +0300

    added images to header

--- src/components/UI/Header/Header.jsx
+++ src/components/UI/Header/Header.jsx
@@ -1,8 +1,34 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import BoxIcon from "../../icons/BoxIcon";
+import Avatar from "/src/assets/avatar.png";
+import Logo from "/src/assets/logo.svg";
+
 
 const Header = () => {
     return ( 
-        <>
+        <header className="header">
+            <div className="header__logo">
+            <img src={Logo} alt="logo" />
+            </div>
+            <div className="header__serch"></div>
+            <nav className="header__nav">
+                <ul className="header__ul">
+                    <li className="header__li">
+                        <Link to={'/'}>Restaurants</Link>
+                    </li>
+                    <li><Link to={'/'}>Deals</Link></li>
+                    <li><Link to={'/'}>My orders</Link></li>
+                </ul>
+            </nav>
+            <div className="header__btn">
+                <div className="headerBusket">
+                    <BoxIcon />
+                    <div className="headerBusket__counter">4</div>
+                </div>
+                <div className="header__profile"></div>
+                <img src={Avatar} alt="avatar" />
+            </div>
+
         <Link to={'/'}>Home              
        </Link> 
        <Link to={'/profile'}>Profile              
@@ -10,7 +36,7 @@ const Header = () => {
 
        
        
-        </>
+        </header>
     )
 }
 
