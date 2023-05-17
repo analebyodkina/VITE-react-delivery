2d3c06e60b7a030cf1d2a5f2bf78a6caaf0e114f
analebyodkina <dp030992cad@gmail.com>
2023-05-17 18:59:05 +0300

    added settings component

---
+++ src/components/profileComponents/SettingsItem/SettingsItem.jsx
@@ -0,0 +1,15 @@
+import "./Settingsitem.scss";
+
+const SettingsItem = () => {
+  return ( 
+     <div className="block">
+        <div className="block__icon"></div>
+        <div className="block__info">
+            <div className="block__title">Account</div>
+            <div className="block__desc">Personal information</div>
+        </div>
+     </div>
+  )
+}
+
+export default SettingsItem
