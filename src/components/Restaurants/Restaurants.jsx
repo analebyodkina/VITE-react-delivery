222741af332857f488322fdfd23a4f150a91e892
analebyodkina <dp030992cad@gmail.com>
2023-05-17 17:53:59 +0300

    started adaptivity of sales component

--- src/components/Restaurants/Restaurants.jsx
+++ src/components/Restaurants/Restaurants.jsx
@@ -17,8 +17,7 @@ const Restaurants = () => {
                 <RestrnItem title="Burgers & Pizza" bg={rest2} time="40-60 min" price="$24 min sum" />  
                 <RestrnItem title="Ninja sushi"  bg={rest3} time="20-40 min" price="$40 min sum" />  
                 <RestrnItem title="Ninja sushi" bg={rest3} time="20-40 min" price="$40 min sum" />  
-                <RestrnItem title="Ninja sushi" bg={rest3} time="20-40 min" price="$40 min sum" />  
-                
+                <RestrnItem title="Ninja sushi" bg={rest3} time="20-40 min" price="$40 min sum" />
                 
             </div>
         </div>
