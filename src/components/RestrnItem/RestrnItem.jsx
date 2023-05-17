e4385a432e7aa0c16175e8ac2596f6abd64383f4
analebyodkina <dp030992cad@gmail.com>
2023-05-17 14:57:02 +0300

    added adaptivity to grid rows

--- src/components/RestrnItem/RestrnItem.jsx
+++ src/components/RestrnItem/RestrnItem.jsx
@@ -68,6 +68,93 @@ const RestrnItem = () => {
         </div>          
           
         </div>  
+
+
+        <div className="restrnItem">
+        <div className="restrnItem__image">
+            <img src={restaur} alt="cafe img" />
+        </div> 
+        <div className="restrnItem__info">
+            <div className="restrnItem__header">
+                <div className="restrnItem__title">Royal Sushi House</div>
+                <div className="restrnItem__busket">
+                    <BoxIcon />
+                </div>
+            </div> 
+            <a href="/" className="restrnItem__section category">                
+                <div className="category__image">
+                    <img src={Sushi} alt="sushi icon" />
+                </div>            
+                <div className="category__title">Suchi</div>                        
+            </a> 
+        </div>          
+          
+        </div>  
+
+        <div className="restrnItem">
+        <div className="restrnItem__image">
+            <img src={restaur} alt="cafe img" />
+        </div> 
+        <div className="restrnItem__info">
+            <div className="restrnItem__header">
+                <div className="restrnItem__title">Royal Sushi House</div>
+                <div className="restrnItem__busket">
+                    <BoxIcon />
+                </div>
+            </div> 
+            <a href="/" className="restrnItem__section category">                
+                <div className="category__image">
+                    <img src={Sushi} alt="sushi icon" />
+                </div>            
+                <div className="category__title">Suchi</div>                        
+            </a> 
+        </div>          
+          
+        </div>  
+
+
+        <div className="restrnItem">
+        <div className="restrnItem__image">
+            <img src={restaur} alt="cafe img" />
+        </div> 
+        <div className="restrnItem__info">
+            <div className="restrnItem__header">
+                <div className="restrnItem__title">Royal Sushi House</div>
+                <div className="restrnItem__busket">
+                    <BoxIcon />
+                </div>
+            </div> 
+            <a href="/" className="restrnItem__section category">                
+                <div className="category__image">
+                    <img src={Sushi} alt="sushi icon" />
+                </div>            
+                <div className="category__title">Suchi</div>                        
+            </a> 
+        </div>          
+          
+        </div>  
+
+
+        <div className="restrnItem">
+        <div className="restrnItem__image">
+            <img src={restaur} alt="cafe img" />
+        </div> 
+        <div className="restrnItem__info">
+            <div className="restrnItem__header">
+                <div className="restrnItem__title">Royal Sushi House</div>
+                <div className="restrnItem__busket">
+                    <BoxIcon />
+                </div>
+            </div> 
+            <a href="/" className="restrnItem__section category">                
+                <div className="category__image">
+                    <img src={Sushi} alt="sushi icon" />
+                </div>            
+                <div className="category__title">Suchi</div>                        
+            </a> 
+        </div>          
+          
+        </div>  
     </>      
         
     
