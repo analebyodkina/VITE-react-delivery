ea54af658d22575943b174f1e8cd7e868bcc5893
analebyodkina <dp030992cad@gmail.com>
2023-05-22 15:38:41 +0300

    added back-end stuff  for avatar component

--- src/components/profileComponents/Avatar/Avatar.jsx
+++ src/components/profileComponents/Avatar/Avatar.jsx
@@ -2,8 +2,23 @@ import AvatarImg from "/src/assets/avatar.png";
 
 import "./Avatar.scss";
 import Button from "../../UI/Button/Button";
+import { useState } from "react";
 
 const Avatar = () => {
+  const [items, setItem] = useState ([
+    {
+        id:20,
+        text:"Change"
+                    
+    },
+    {
+        id:21,
+        text:"Remove",
+        color:"greyNoBorder"
+                    
+    },
+
+])
   return ( 
     <div className="avatar">
         <h4 className="heading avatar__title">Avatar</h4>
@@ -11,8 +26,15 @@ const Avatar = () => {
             <div className="avatar__image">
                 <img src={AvatarImg} alt="avatar" />
             </div>
-            <Button text ="Change" />
-            <Button text ="Remove" color="greyNoBorder" />
+            {/* <Button text="Change" />
+            <Button text="Remove" color="greyNoBorder" /> */}
+
+            {items && items.map((item,index) => {
+                return (
+                    <Button key={index} text={item.text} color={item.color} />
+                )
+              }
+            )}
         </div>
     </div>
   )
