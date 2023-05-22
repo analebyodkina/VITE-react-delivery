e15bc7e7caee65172cf2b2a6edeec48d4b2319c9
analebyodkina <dp030992cad@gmail.com>
2023-05-22 16:08:46 +0300

    added back-end stuff  for profileFields component

--- src/components/profileComponents/ProfileFields/ProfileFields.jsx
+++ src/components/profileComponents/ProfileFields/ProfileFields.jsx
@@ -1,14 +1,48 @@
-
 import TextInput from "../../UI/TextInput/TextInput";
 import "./ProfileFields.scss";
+import { useState } from "react";
 
 const ProfileFields = () => {
+  const [items, setItem] = useState ([
+    {
+        id:28,
+        title:"First name",
+        type:"text",
+        placeholder:"Jane"                  
+    },
+    {
+        id:29,
+        title:"Last name",
+        type:"text",
+        placeholder:"Robertson"               
+    },
+    {
+      id:30,
+      title:"Email",
+      type:"email",
+      placeholder:"jane.robertson@example.com"               
+    },
+    {
+      id:31,
+      title:"Phone number",
+      type:"tel",
+      placeholder:"(217) 555-0113"              
+  },
+
+])
   return ( 
      <div className="profileFields">
-        <TextInput title="First name" type="text" placeholder="Jane"/>
+        {/* <TextInput title="First name" type="text" placeholder="Jane"/>
         <TextInput title="Last name" type="text" placeholder="Robertson"/>
         <TextInput title="Email" type="email" placeholder="jane.robertson@example.com"/>
-        <TextInput title="Phone number" type="tel" placeholder="(217) 555-0113"/>
+        <TextInput title="Phone number" type="tel" placeholder="(217) 555-0113"/> */}
+
+        {items && items.map((item,index) => {
+            return (
+                <TextInput key={index} title={item.title} type={item.type} placeholder={item.placeholder} />
+            )
+        }
+        )}
         
     </div>
   )
