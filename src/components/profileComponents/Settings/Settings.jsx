674552f3d91312556d41a037d8fea57ac01f5e1a
analebyodkina <dp030992cad@gmail.com>
2023-05-22 17:11:00 +0300

    added back-end stuff for settings section

--- src/components/profileComponents/Settings/Settings.jsx
+++ src/components/profileComponents/Settings/Settings.jsx
@@ -12,26 +12,27 @@ const Settings = () => {
         id:16,
         title:"Account",
         desc:"Personal information",      
-        
+        icon:<UserIcon />
                     
     },
     {
         id:17,
         title:"Address",
         desc:"Shippings addresses",
+        icon:<PointIcon />
                     
     },
     {
       id:18,
       title:"Payment method",
       desc:"Connected credit cards",
-                  
+      icon:<CardIcon />           
   },
   {
     id:19,
     title:"Security",
     desc:"Password, 2FA",
-                
+    icon:<ShieldIcon />          
 },
 
 ])
@@ -39,12 +40,18 @@ const Settings = () => {
      <div className="settings">
         <div className="title settings__title">Settings</div>
         <div className="settings__list" >
-          <SettingsItem title="Account" desc="Personal information" icon={<UserIcon />}/>
-
-
+          {/* <SettingsItem title="Account" desc="Personal information" icon={<UserIcon />}/>
           <SettingsItem title="Address" desc="Shippings addresses" icon={<PointIcon />}/>
           <SettingsItem title="Payment method" desc="Connected credit cards" icon={<CardIcon />}/>
-          <SettingsItem title="Security" desc="Password, 2FA" icon={<ShieldIcon />} />
+          <SettingsItem title="Security" desc="Password, 2FA" icon={<ShieldIcon />} /> */}
+
+          {items && items.map((item,index) => {
+              return (
+                  <SettingsItem key={index} title={item.title} desc={item.desc} subtitle={item.subtitle} icon={item.icon} />
+              )
+          }
+          )}
+
         </div>
     </div>
   )
