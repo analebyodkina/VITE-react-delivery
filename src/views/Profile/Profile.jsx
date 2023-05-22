c1d3f23ec55e2189d3d989bff97920f6793b47b6
analebyodkina <dp030992cad@gmail.com>
2023-05-22 15:19:27 +0300

    added back-end stuff  for profile component

--- src/views/Profile/Profile.jsx
+++ src/views/Profile/Profile.jsx
@@ -2,18 +2,34 @@ import Account from "../../components/profileComponents/Account/Account";
 import Settings from "../../components/profileComponents/Settings/Settings";
 
 import "./Profile.scss";
+import { useState } from "react";
 
 const Profile = () => {
+  const [items, setItem] = useState ([
+    {
+        id:15,
+        title:"Account",
+        subtitle:"Personal information"                     
+    }    
+
+])
   return ( 
-     <div className="profile container">
+    <div className="profile container">
       <div className="profile__settings">
         <Settings />
       </div>
       <div className="profile__account">
-        <Account title="Account" subtitle="Personal information"/>
-      </div>
+        {/* <Account title="Account" subtitle="Personal information"/> */}
 
-     </div>
+        {items && items.map((item,index) => {
+            return (                        
+              <Account key={index} title={item.title} subtitle={item.subtitle}/>
+            )
+        }
+        )}
+
+      </div>
+    </div>
   )
 }
 
