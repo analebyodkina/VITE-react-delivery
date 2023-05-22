2231ce71f0a7f2826cacc9a10a8fc0b51d8ac2ab
analebyodkina <dp030992cad@gmail.com>
2023-05-22 16:12:17 +0300

    added back-end stuff  for profileFooter component

--- src/components/profileComponents/ProfileFooter/ProfileFooter.jsx
+++ src/components/profileComponents/ProfileFooter/ProfileFooter.jsx
@@ -1,15 +1,38 @@
 import Button from "../../UI/Button/Button";
-
+import { useState } from "react";
 import "./ProfileFooter.scss";
 
 const ProfileFooter = () => {
+  const [items, setItem] = useState ([
+    {
+        id:32,
+        text:"Log out",
+        color:"red"                   
+    },
+    {
+        id:33,
+        text:"Discard changes",
+        color:"grey"             
+    },
+    {
+      id:33,
+      text:"Save changes",
+      color:"blue"             
+  },
+
+])
   return ( 
      <div className="footer">      
-      <Button text ="Log out" color="red" />
-      <Button text ="Discard changes" color="grey"/>
-      <Button text ="Save changes" color="blue"/>
+      {/* <Button text="Log out" color="red" />
+      <Button text="Discard changes" color="grey"/>
+      <Button text="Save changes" color="blue"/> */}
         
-        
+      {items && items.map((item,index) => {
+          return (
+              <Button key={index} text={item.text} color={item.color} />
+          )
+       }
+      )}
     </div>
   )
 }
