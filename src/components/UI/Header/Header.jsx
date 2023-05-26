a5e02f3ddabccdc2ef092a0368193d1f131a5439
analebyodkina <dp030992cad@gmail.com>
2023-05-26 15:00:21 +0300

    header burger doesn't work

--- src/components/UI/Header/Header.jsx
+++ src/components/UI/Header/Header.jsx
@@ -3,22 +3,32 @@ import BoxIcon from "../../icons/BoxIcon";
 import Avatar from "/src/assets/avatar.png";
 import Logo from "/src/assets/logo.svg";
 import SearchInput from "../SearchInput/SearchInput";
+import { useState } from 'react';
 
 import style from "./Header.module.scss"
 
 
 const Header = () => {
+  function navToggle() {
+    
+    var html = document.getElementById("html");
+	  html.classList.toggle("overflow-y-hidden")
+    var body = document.getElementById("body");
+	  body.classList.toggle("overflow-y-hidden");
+      var links = document.getElementById('headerUl');
+      links.classList.toggle("active");    
+  }
     return ( 
         <header className={style.header}>
             <div className={`container ${style.header__container}`}>
                 <Link to={'/'} className={style.header__logo}>
                     <img src={Logo} alt="food delivery" />
                 </Link>
-                <div className={style.header__serch}>
+                <div className={style.header__search}>
                     <SearchInput />
                 </div>
                 <nav className={style.header__nav}>
-                    <ul className={style.header__ul}>
+                    <ul className={style.header__ul} id='headerUl'>
                         <li className={style.header__li}>
                             <Link to={'/'}>Restaurants</Link>
                         </li>
@@ -33,6 +43,11 @@ const Header = () => {
                 <Link to={'/profile'} className={style.header__profile}>
                     <img src={Avatar} alt="avatar" />
                 </Link>
+                <div className={style.header__burger}>
+                    <span className="material-symbols-outlined" onClick={navToggle}>
+                    menu
+                    </span>
+                </div>
             </div>
                     
                 
